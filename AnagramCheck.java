9cb5f2a92ba1f8007753029dc67996507e11bdc4
ramya110704-design <ramya110704@gmail.com>
2026-01-20 22:13:59 +0530

    AnagramCheck.java File created

---
+++ AnagramCheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class AnagramCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First String: ");
+        String str1 = sc.nextLine();
+        System.out.println("Enter the Second String: ");
+        String str2 = sc.nextLine();
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+        char[] arr1=str1.toCharArray();
+        char[] arr2=str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if(Arrays.equals(arr1,arr2)){
+            System.out.println("The Strings are Anagrams");
+        }
+        else{
+             System.out.println("The Strings are not Anagrams");
+        }
+
+    }
+    
+}
