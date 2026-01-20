34090f59671a96b16420dcaaec9d36d012ed68c5
ramya110704-design <ramya110704@gmail.com>
2026-01-20 20:30:50 +0530

    ReverseString.java file created

---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        char[] ch = str.toCharArray();
+        int i=0,j= ch.length-1;
+        while(i < j){
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println("Reversed String: "+ new String(ch));
+    }
+    
+}
