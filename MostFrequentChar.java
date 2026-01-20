cc9ba33e918c2851c850f24048cbc4b3fecd4ebf
ramya110704-design <ramya110704@gmail.com>
2026-01-20 21:52:50 +0530

    MostFrequentChar.java file created

---
+++ MostFrequentChar.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MostFrequentChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        int maxcount=0;
+        char mostFrequent='';
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            int count=0;
+            for(int j=0;j<str.length();j++){
+                if(str.charAt(j) == ch){
+                    count++;
+                }
+            }
+            if(count > maxcount){
+                maxcount = count;
+                mostFrequent = ch;
+            }
+        }
+        System.out.println("Most Frequent character: "+ mostFrequent);
+        System.out.println("Frequency: "+ maxcount);
+        sc.close();
+    }
+    
+}
