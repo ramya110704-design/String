a62077eea977b19b454b4666cafac8a3e1c89525
ramya110704-design <ramya110704@gmail.com>
2026-01-20 22:30:47 +0530

    WhiteSpace.java file created

---
+++ WhiteSpace.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class WhiteSpace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String:");
+        String input= sc.nextLine();
+        String result= input.replaceAll("\\s","");
+        System.out.println("String after removing spaces:");
+        System.out.println(result);
+    }
+    
+}
