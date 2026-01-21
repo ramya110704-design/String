834bff60ca4336848c2838ee35c583e83c29441b
ramya110704-design <ramya110704@gmail.com>
2026-01-21 10:59:42 +0530

    Capitalizeword.java file created

---
+++ Capitalizeword.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Capitalizeword {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String input = sc.nextLine();
+        String[] words = input.split(" ");
+        String result = " ";
+        for(int i=0;i<words.length;i++){
+            String word = words[i];
+            if(word.length()> 0){
+                result = result + word.substring(0,1).toUpperCase()+ word.substring(1)+" ";
+            }
+        }
+        System.out.println("After Capitalization: ");
+        System.out.println(result.trim());
+    }
+    
+}
